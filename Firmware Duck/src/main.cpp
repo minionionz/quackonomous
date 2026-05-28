00062e55ebbf33ca44be3e7629a75639953f069a
mallo321123 <franz.huber1@gmx.de>
2026-05-28 23:18:26 +0200

    add simple test Data transver from remote to Duck

--- Firmware Duck/src/main.cpp
+++ Firmware Duck/src/main.cpp
@@ -1,18 +1,78 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
 
-// put function declarations here:
-int myFunction(int, int);
+struct __attribute__((packed)) StickData
+{
+  uint16_t x;
+  uint16_t y;
+};
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Callback-Funktion, die automatisch aufgerufen wird, wenn eine ESP-NOW-Nachricht empfangen wird
+// Sie erhält die MAC-Adresse des Senders und die empfangenen Daten.
+void onDataRecv(const uint8_t *mac_addr, const uint8_t *data, int len)
+{
+  // Gib "Von: " aus, gefolgt von der MAC-Adresse des Senders
+  Serial.print("Von: ");
+
+  // MAC address des Senders ausgeben
+  for (int i = 0; i < 6; i++)
+  {
+    Serial.printf("%02X", mac_addr[i]);
+    if (i < 5)
+      Serial.print(":");
+  }
+
+  Serial.print(" | StickData: ");
+  if (len >= static_cast<int>(sizeof(StickData)))
+  {
+    StickData stickData;
+    memcpy(&stickData, data, sizeof(StickData));
+    Serial.print("x=");
+    Serial.print(stickData.x);
+    Serial.print(", y=");
+    Serial.print(stickData.y);
+  }
+  else
+  {
+    Serial.print("ungültige Länge (");
+    Serial.print(len);
+    Serial.print(")");
+  }
+
+  Serial.print(" ");
+  Serial.println();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Initialisierung des ESP32
+void setup()
+{
+  Serial.begin(115200);
+
+  // Setze den WiFi-Modus auf Station (Empfänger-Modus)
+  WiFi.mode(WIFI_STA);
+  // Zur Information: Die MAC-Adresse des ESP32 wird hier ausgegeben,
+  // damit du sie für den Sender verwenden kannst
+  Serial.print("Empfänger MAC: ");
+  Serial.println(WiFi.macAddress());
+
+  // Initialisiere ESP-NOW
+  if (esp_now_init() != ESP_OK)
+  {
+    Serial.println("ESP-NOW Fehler");
+    while (true)
+      delay(1000);
+  }
+
+  // Registriere die Callback-Funktion, die aufgerufen wird, wenn Daten empfangen werden
+  // onDataRecv wird automatisch aufgerufen, wenn eine ESP-NOW-Nachricht ankommt
+  esp_now_register_recv_cb(onDataRecv);
+
+  // Bestätige, dass der Empfänger bereit ist
+  Serial.println("Empfänger bereit");
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  delay(1000);
 }
--- Firmware Remote/src/main.cpp
+++ Firmware Remote/src/main.cpp
@@ -1,18 +1,60 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Diese MAC-Adresse muss zur MAC deines Empfängers passen.
+uint8_t empfaengerMac[] = {0xD4, 0xE9, 0xF4, 0xBD, 0xB5, 0x0C};
+
+int STICK_X = 1;
+int STICK_Y = 0;
+
+struct __attribute__((packed)) StickData
+{
+  uint16_t x;
+  uint16_t y;
+};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+
+  // Eigene MAC anzeigen (praktisch zum Kopieren).
+  Serial.print("Sender MAC: ");
+  Serial.println(WiFi.macAddress());
+
+  if (esp_now_init() != ESP_OK)
+    Serial.println("Fehler: ESP-NOW konnte nicht gestartet werden.");
+
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, empfaengerMac, 6);
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    Serial.println("Fehler: Empfaenger konnte nicht hinzugefuegt werden.");
+
+  Serial.println("ESP-NOW ready");
+
+  pinMode(STICK_X, INPUT);
+  pinMode(STICK_Y, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  uint16_t xValue = analogRead(STICK_X);
+  uint16_t yValue = analogRead(STICK_Y);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  StickData data = {xValue, yValue};
+
+  esp_err_t result = esp_now_send(empfaengerMac, (uint8_t *)&data, sizeof(data));
+
+  if (result == ESP_OK) {
+    Serial.println("Nachricht gesendet");
+  } else {
+    Serial.println("Fehler beim Senden der Nachricht");
+  }
+
+  Serial.print("Stick X: ");
+  Serial.print(xValue);
+  Serial.print(" Stick Y: ");
+  Serial.println(yValue);
+  delay(100);
 }
