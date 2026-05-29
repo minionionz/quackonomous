ace361f68261b1f9289fcf6c81c1884235cbfe54
Ruben Klein <ruben.klein@st.oth-regensburg.de>
2026-05-29 16:07:52 +0200

    Rewrite it for the ESP8266

--- FirmwareRemote/src/main.cpp
+++ FirmwareRemote/src/main.cpp
@@ -1,14 +1,22 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <esp_now.h>
+#include <ESP8266WiFi.h>
+#include <espnow.h>
 #include "common.h"
 
+#ifndef ESP32_COMPAT_H
+#define ESP32_COMPAT_H
+typedef int esp_err_t;
+#define ESP_OK 0
+#define ESP_FAIL -1
+#define pdMS_TO_TICKS(ms) (ms) // no FreeRTOS ticks on ESP8266
+#endif
 
-const int STICK_X = 0;
-const int STICK_Y = 1;
+const int STICK_X = A0;
+const int STICK_Y = D1;
 
 // Diese MAC-Adresse muss zur MAC deines Empfängers passen.
-uint8_t empfaengerMac[] = {0xD4, 0xE9, 0xF4, 0xBD, 0xB5, 0x0C};
+// uint8_t empfaengerMac[] = {0xD4, 0xE9, 0xF4, 0xBD, 0xB5, 0x0C};
+uint8_t empfaengerMac[] = {0x14, 0x63, 0x93, 0xC8, 0x92, 0xC0};
 
 
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
@@ -32,14 +40,12 @@ void setup() {
   if (esp_now_init() != ESP_OK)
     Serial.println("Fehler: ESP-NOW konnte nicht gestartet werden.");
 
-  // esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   // Register receiver as slave
-  esp_now_peer_info_t peer_info = {};
-  memcpy(&peer_info.peer_addr, empfaengerMac, 6);
-  if (esp_now_add_peer(&peer_info) != ESP_OK)
+  if (esp_now_add_peer(empfaengerMac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0) != ESP_OK)
     Serial.println("Fehler: Empfaenger konnte nicht hinzugefuegt werden.");
 
-  // esp_now_register_send_cb(OnDataSent);
+  esp_now_register_send_cb(OnDataSent);
 
   Serial.println("ESP-NOW ready");
 
@@ -59,12 +65,7 @@ void loop() {
   Message data;
   data.msg_type = STICK_DATA;
   data.data.stick_data = StickData { .x = xValue, .y = yValue };
-  auto res = esp_now_send(NULL, (uint8_t *)&data, sizeof(data));
-  if (res == ESP_OK) {
-    Serial.println("[OK] Nachricht gesendet");
-  } else {
-    Serial.println("[ERR] Fehler beim Senden der Nachricht");
-  }
+  esp_now_send(empfaengerMac, (uint8_t *)&data, sizeof(data));
 
   delay(100);
 }
