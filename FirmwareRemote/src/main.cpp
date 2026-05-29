9440b462b279b0b2cfe3eebd956f110f69b48b63
Ruben Klein <ruben.klein@st.oth-regensburg.de>
2026-05-29 17:04:28 +0200

    Rewrite firmware for remote to better switch between esp types
    Used pio envs

--- FirmwareRemote/src/main.cpp
+++ FirmwareRemote/src/main.cpp
@@ -1,31 +1,36 @@
 #include <Arduino.h>
+
+#ifdef ESP8266
+/* ESP8266 */
+
 #include <ESP8266WiFi.h>
 #include <espnow.h>
-#include "common.h"
-
-#ifndef ESP32_COMPAT_H
-#define ESP32_COMPAT_H
+// Compat variables
 typedef int esp_err_t;
 #define ESP_OK 0
 #define ESP_FAIL -1
 #define pdMS_TO_TICKS(ms) (ms) // no FreeRTOS ticks on ESP8266
-#endif
 
 const int STICK_X = A0;
 const int STICK_Y = D1;
 
+#else
+/* ESP32 */
+#include <WiFi.h>
+#include <esp_now.h>
+const int STICK_X = 0;
+const int STICK_Y = 1;
+
+#endif
+#include "common.h"
+
+
+uint8_t broadcast[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 // Diese MAC-Adresse muss zur MAC deines Empfängers passen.
-// uint8_t empfaengerMac[] = {0xD4, 0xE9, 0xF4, 0xBD, 0xB5, 0x0C};
-uint8_t empfaengerMac[] = {0x14, 0x63, 0x93, 0xC8, 0x92, 0xC0};
+uint8_t empfaengerMac[] = {0xD4, 0xE9, 0xF4, 0xBD, 0xB5, 0x0C};
+// uint8_t empfaengerMac[] = {0x14, 0x63, 0x93, 0xC8, 0x92, 0xC0};
 
-
-void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
-  if (sendStatus == ESP_OK) {
-    Serial.println("[OK] Nachricht gesendet");
-  } else {
-    Serial.println("[ERR] Fehler beim Senden der Nachricht");
-  }
-}
+uint8_t *active_mac = broadcast;
 
 void setup() {
   Serial.begin(115200);
@@ -40,12 +45,17 @@ void setup() {
   if (esp_now_init() != ESP_OK)
     Serial.println("Fehler: ESP-NOW konnte nicht gestartet werden.");
 
-  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+#ifdef ESP8266
   // Register receiver as slave
-  if (esp_now_add_peer(empfaengerMac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0) != ESP_OK)
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  if (esp_now_add_peer(active_mac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0) != ESP_OK) {
+#else
+  esp_now_peer_info_t peer_info = {};
+  memcpy(&peer_info.peer_addr, active_mac, 6);
+  if (esp_now_add_peer(&peer_info) != ESP_OK) {
+#endif // ESP8266
     Serial.println("Fehler: Empfaenger konnte nicht hinzugefuegt werden.");
-
-  esp_now_register_send_cb(OnDataSent);
+  }
 
   Serial.println("ESP-NOW ready");
 
@@ -65,7 +75,12 @@ void loop() {
   Message data;
   data.msg_type = STICK_DATA;
   data.data.stick_data = StickData { .x = xValue, .y = yValue };
-  esp_now_send(empfaengerMac, (uint8_t *)&data, sizeof(data));
+  auto res = esp_now_send(NULL, (uint8_t *)&data, sizeof(data));
+  if (res == ESP_OK) {
+    Serial.println("[OK] Nachricht gesendet");
+  } else {
+    Serial.println("[ERR] Fehler beim Senden der Nachricht");
+  }
 
   delay(100);
 }
